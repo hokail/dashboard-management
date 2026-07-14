5c898f93d6fbf9753976edb38e588b3fb1fb5b29
l <1303604748@qq.com>
2026-07-14 12:13:14 +0800

    feat：页面push至github page

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
 
 // 创建路由实例
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -20,4 +20,5 @@ export default defineConfig({
       enable: true,
     }),
   ],
+  base: '/dashboard/'
 })
